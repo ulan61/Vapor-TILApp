cc6be3eb096c53587622e25bf14d4e38d3f07660
Ulan <ulannurmamatov@gmail.com>
2018-09-22 22:23:30 +0600

    After updatin

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ let package = Package(
     name: "TILApp",
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git",
-                 from: "3.0.0"),
+                 from: "3.1.0"),
 
         .package(url: "https://github.com/vapor/fluent-postgresql.git",
                  from: "1.0.0-rc"),
         ],
-    targets: [
+    targets: [а
         .target(name: "App", dependencies: ["FluentPostgreSQL",
                                             "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
