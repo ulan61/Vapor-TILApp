afc42bbf7aed36b09e670b6060efab80b60f727b
Ulan <ulannurmamatov@gmail.com>
2018-09-22 23:52:41 +0600

    Updating

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0"),
 
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0")
+        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc")
         ],
     targets: [
         .target(name: "App", dependencies: ["FluentPostgreSQL",
