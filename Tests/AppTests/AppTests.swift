7619722f4035769bd30b1e3f49bc60b86d8d6fad
Ulan <ulannurmamatov@gmail.com>
2018-09-22 16:39:59 +0600

    first commit

---
+++ Tests/AppTests/AppTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+
+final class AppTests: XCTestCase {
+    func testNothing() throws {
+        // add your tests here
+        XCTAssert(true)
+    }
+
+    static let allTests = [
+        ("testNothing", testNothing)
+    ]
+}
